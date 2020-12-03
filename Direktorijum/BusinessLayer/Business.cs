9c5cba5f0730f9d5475c15c3bfd4e48324d07786
Djordje Djokovic <djokovicdjordje70@gmail.com>
2020-12-03 21:45:21 +0100

    Napravljen BusinessLayer i u njemu klasa Business sa dve metode

---
+++ Direktorijum/BusinessLayer/Business.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Business
+    {
+        private readonly LibraryRepository libraryRepository;
+
+        public Business()
+        {
+            this.libraryRepository = new LibraryRepository();
+        }
+
+        public List<Library> GetAllBooks()
+        {
+            return this.libraryRepository.GetAllBooks();
+        }
+
+        public List<Library> GetBooks(int x)
+        {
+            x = 50;
+            return this.libraryRepository.GetAllBooks()
+                .Where(l => l.NumberOfPages > x).ToList();
+        }
+    }
+}

f24b305cb636b77c0a448f6e599c5dc265a08d2c
R217-12\student <example@gmail.com>
2020-12-03 21:20:57 +0100

    Created DataAccess and Completed

---
+++ PresentationLayer/DataAccessLayer/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class BookRepository
+    {
+        public int InsertBook(Book book)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(Constants.connString))
+            {
+                sqlConn.Open();
+                SqlCommand comm = new SqlCommand(
+                    string.Format("INSERT INTO dbo.Books VALUES('{0}',{1})", book.Name, book.NumberOfpages),
+                    sqlConn);
+
+                return comm.ExecuteNonQuery();
+            }
+
+        }
+        public List<Book> GetAllBooks()
+        {
+            using (SqlConnection sqlConn = new SqlConnection(Constants.connString))
+            {
+                sqlConn.Open();
+                List<Book> list = new List<Book>();
+                SqlCommand comm = new SqlCommand(
+                    "SELECT * FROM dbo.Books",
+                    sqlConn);
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    //napisi dodavanje
+
+                }
+                return list;
+            }
+        }
+    }
+}

15250b7d159100387fd23a01360c8a99903e0fff
Nikola Tomic <50-2018@ftn.edu.rs>
2020-12-03 21:36:55 +0100

    Kreirane metode u BusinessLayer-u

---
+++ PresentationLayer/BusinessLayer/BookBusiness.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLayer
+{
+    public class BookBusiness
+    {
+        private readonly BookRepository bookRepository;
+
+        public BookBusiness()
+        {
+            this.bookRepository = new BookRepository();
+        }
+
+        public bool InsertBook(Book b)
+        {
+            if (this.bookRepository.InsertBook(b) > 0)
+                return true;
+            return false;
+        }
+
+        public List<Book> GetAllBooksGTPages(int nop)
+        {
+            return this.bookRepository.GetAllBooks().Where(item => item.NumberOfPages > 50).ToList();
+        }
+
+    }
+}

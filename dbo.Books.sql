1a28bc3ee91f9e182070edaeef281f0845bb9b17
Aleksa Makljenovic <makljenovic99@gmail.com>
2020-12-03 20:15:10 +0000

    Kreirani biznis, data i prezentacioni slojevi kao i klasa za mapiranje baze. Dodat sql kod za kreiranje tabele.

---
+++ dbo.Books.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (50) NOT NULL,
+    [NumberOfPages] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
